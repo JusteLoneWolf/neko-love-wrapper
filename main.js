0d8d858b90a74f93965c6b868f4556c5edc081a0
zechaos031 <36123003+zechaos031@users.noreply.github.com>
2019-12-10 13:55:13 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 module.exports = (key) => new Promise((resolve, reject) => {
     const {get} = require('axios')
     if (!key) {
-        throw reject(`[Neko-Love-Wrapper] No keywords provided`);
+        throw reject(`[Neko-Love] No keywords provided`);
     }
     return get(`https://neko-love.xyz/api/v1/${key}`).then((res) => {
         resolve(res.data.url);
-    }).catch(error => { reject(`[Neko-Love-Wrapper] Internal error: ${error.toString()}`); });
+    }).catch(error => {
+        if(error.toString().includes('404')){
+            reject('[Neko-Love] Unavailable endpoint\n\nEndpoint Available:\n neko,nekolewd,kitsune,pat,hug,waifu,cry,kiss,slap\n\n')
+        }else {
+            reject(`[Neko-Love] Internal error: ${error.toString()}`);
+        }
+    })
 });
